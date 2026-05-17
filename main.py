608d4c2359756a9044b70f320ef40dde0a616acc
Aksh Jadiya <aksh.jadiya.real@gmail.com>
2026-05-17 10:28:50 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#Studen Intro Generator
+
+print("Welcome to Future Developer Profile Generator")
+name = input("What is your name ?")
+age = input("What is your age ?")
+dream_company = input("What is your Dream Company ?")
+favourite_language = input("What is your favourite coding language ?")
+work_hours = input("How many hours are you willing to work hard everyday ?")
+work_years = input("How many years are you willing to work hard ?")
+
+print("-----Future Developer Profile-----")
+print("Name : " + name)
+print("Age : " + age)
+print("Dream Company : " + dream_company)
+print("Favourite Coding Language : " + favourite_language)
+print("Willing to work hard for " + work_hours + " hours everyday")
+
+print("In " + work_years + " years, " + name + " will be a great " + favourite_language + " developer working at " + dream_company)
